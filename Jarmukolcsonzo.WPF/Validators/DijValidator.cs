d6a5951ff68600f303766e6ed34f7d94f1a1b222
sze4kalend <sze4kalend@vasvari.org>
2026-03-12 19:45:53 +0100

    validálás vizsgán(kiegészítés 21. sorral pl...)

---
+++ Jarmukolcsonzo.WPF/Validators/DijValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Jarmukolcsonzo.WPF.Validators
+{
+    public class DijValidator : ValidationRule //Vizsgán validálás
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            int.TryParse(value.ToString(), out int dij);
+
+            if(dij < 0)
+            {
+                return new ValidationResult(false, "A díj nem lehet negatív érték");  
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+}

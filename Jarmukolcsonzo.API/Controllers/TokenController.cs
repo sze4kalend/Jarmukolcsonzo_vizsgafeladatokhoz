d8b7c030f42d9c4b9e894c6eedebf6cd47b8e9c0
sze4kalend <sze4kalend@vasvari.org>
2026-03-30 17:39:17 +0200

    órán tokencontrollercs

---
+++ Jarmukolcsonzo.API/Controllers/TokenController.cs
@@ -0,0 +1,19 @@
+﻿using Jarmukolcsonzo.Shared.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jarmukolcsonzo.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    // Post: api/Token/Login
+    {
+        [HttpPost("Login")]
+        public ActionResult Login(LoginDto login)
+        {
+
+            return Ok();
+        }
+    }
+}

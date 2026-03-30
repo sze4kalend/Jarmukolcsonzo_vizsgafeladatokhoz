e9ffe97a6028bf69f21f917cdda87efde2bda6d5
sze4kalend <sze4kalend@vasvari.org>
2026-03-30 17:55:03 +0200

    bcrypt,hash

--- Jarmukolcsonzo.API/Controllers/TokenController.cs
+++ Jarmukolcsonzo.API/Controllers/TokenController.cs
@@ -1,18 +1,35 @@
-﻿using Jarmukolcsonzo.Shared.DTOs;
+﻿using Jarmukolcsonzo.API.Data;
+using Jarmukolcsonzo.API.Models;
+using Jarmukolcsonzo.Shared.DTOs;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Jarmukolcsonzo.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class TokenController : ControllerBase
-    // Post: api/Token/Login
     {
-        [HttpPost("Login")]
-        public ActionResult Login(LoginDto login)
+        private readonly JKContext _context;
+        public TokenController(JKContext context)
         {
-
+            _context = context;
+        }
+    // Post: api/Token/Login
+    
+    
+        [HttpPost("Login")]
+        public async Task<ActionResult> Login(LoginDto login)
+        {
+            //Felhasználó keresésée
+            Felhasznalo? dbUser = await _context.felhasznalok.Include(x => x.szerepkor).FirstOrDefaultAsync(x => x.felhasznalonev == login.Username);
+            if (dbUser == null)
+            {
+                return Unauthorized("A felhasználó nincs regisztrálva");//401
+            }
+            //Jelszó ellenőrzése
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(login.Password);
             return Ok();
         }
     }
